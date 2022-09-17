e78c6b892cf352a6bc6e1c8f5a081021d5e3e2b7
almasm <amyrzatay3@gatech.edu>
2022-09-17 15:39:47 -0400

    Updated readme

--- src/BarGraphSimple.js
+++ src/BarGraphSimple.js
@@ -5,35 +5,24 @@ const margin = { top: 10, right: 10, bottom: 50, left: 50 };
 const width = 500 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-function AxisBottom({ scale, transform }) {
+const CreateAxis = ({ axisDirection, scale, transform }) => {
   const ref = useRef(null);
 
   useEffect(() => {
     if (ref.current) {
-      select(ref.current).call(axisBottom(scale));
+      select(ref.current).call(axisDirection(scale));
     }
   }, [scale]);
 
   return <g ref={ref} transform={transform} />;
-}
+};
 
-function AxisLeft({ scale }) {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (ref.current) {
-      select(ref.current).call(axisLeft(scale));
-    }
-  }, [scale]);
-
-  return <g ref={ref} />;
-}
-
-function Bars({ data, height, scaleX, scaleY }) {
+const CreateBars = ({ data, height, scaleX, scaleY }) => {
   return (
     <>
       {data.map(({ value, label }) => (
         <rect
+          key={label}
           x={scaleX(label)}
           y={scaleY(value)}
           width={scaleX.bandwidth()}
@@ -43,7 +32,7 @@ function Bars({ data, height, scaleX, scaleY }) {
       ))}
     </>
   );
-}
+};
 
 const BarGraphSimple = ({ data }) => {
   const data_labels = data.map(({ label }) => label);
@@ -59,9 +48,19 @@ const BarGraphSimple = ({ data }) => {
       height={height + margin.top + margin.bottom}
     >
       <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <AxisBottom scale={scaleX} transform={`translate(0, ${height})`} />
-        <AxisLeft scale={scaleY} />
-        <Bars data={data} height={height} scaleX={scaleX} scaleY={scaleY} />
+        <CreateAxis
+          axisDirection={axisBottom}
+          scale={scaleX}
+          transform={`translate(0, ${height})`}
+        />
+        <CreateAxis axisDirection={axisLeft} scale={scaleY} />
+
+        <CreateBars
+          data={data}
+          height={height}
+          scaleX={scaleX}
+          scaleY={scaleY}
+        />
       </g>
     </svg>
   );
